a42bd8c123ed626cda0224bfae144f04f9c6c878
Filip Mularski <mularskif@gmail.com>
2024-02-27 15:54:19 +0100

    [feat] added items icons

--- rpg/models.py
+++ rpg/models.py
@@ -131,6 +131,7 @@ class Item(models.Model):
     boosted_stat = models.CharField(max_length=20, choices=StatisticsChoices.choices)
     value = models.IntegerField()
     character = models.ForeignKey(Character, on_delete=models.CASCADE, related_name="items")
+    icon = models.ImageField(upload_to="items/", null=True, blank=True)
 
     def __str__(self):
         return self.name
