8f16aaf694cef60681ad48e3404d0c49130dd521
Filip Mularski <mularskif@gmail.com>
2024-02-28 16:26:51 +0100

    updated command populate_databse

--- rpg/management/commands/populate_database.py
+++ rpg/management/commands/populate_database.py
@@ -2,6 +2,8 @@ from django.contrib.auth import get_user_model
 from django.core.management import call_command
 from django.core.management.base import BaseCommand
 
+from rpg import models
+
 User = get_user_model()
 
 
@@ -19,12 +21,16 @@ class Command(BaseCommand):
     ]
 
     def _create_admin(self):
-        User.objects.create_superuser(username="admin", password="admin")
+        admin_exists = User.objects.filter(username="admin").exists()
+        if not admin_exists:
+            User.objects.create_superuser(username="admin", password="admin")
 
     def handle(self, *args, **options):
         self._create_admin()
 
-        for fixture in self.fixtures:
-            call_command("loaddata", fixture)
+        database_populated = models.Guild.objects.exists()
+        if not database_populated:
+            for fixture in self.fixtures:
+                call_command("loaddata", fixture)
 
         self.stdout.write(self.style.SUCCESS("Database populated."))
